72458377acfae07c0d1c22c432dc1a228e02681b
reynoldyehezkiel <reynoldyehezkiel25@gmail.com>
2023-11-03 09:27:01 +0700

    chore: create spring project

---
+++ src/main/java/com/nutech/AssignmentApiProgrammerApplication.java
@@ -0,0 +1,13 @@
+package com.nutech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentApiProgrammerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentApiProgrammerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nutech/AssignmentApiProgrammerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentApiProgrammerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
